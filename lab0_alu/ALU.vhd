4f6d8517fa86fef9b67b3e9a623d1eb8429087c8
Aaron Valdes <axv725@miami.edu>
2021-02-02 08:24:56 -0500

    Creating entity

--- lab0_alu/ALU.vhd
+++ lab0_alu/ALU.vhd
@@ -1,3 +1,15 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity ALU is
+    port(
+        CLK:in std_logic;
+        LOAD:in std_logic_vector(5 downto 0);
+        SEL:in std_logic_vector(7 downto 0);
+        DMD:inout std_logic_vector(15 downto 0);
+        R:inout std_logic_vector(15 downto 0);
+        PMD:in std_logic_vector()
+    )
+end ALU;
